b036bb792b5a4463a0e84914ef3ff2161afe0f7d
Michal Budzynski <michal@virtualdesign.pl>
2014-07-17 18:15:54 +0200

    [FILESTUBE-CLIENT] - :circus_tent: more mocks for tests

--- tests/filestube-client-test.js
+++ tests/filestube-client-test.js
@@ -4,34 +4,36 @@ var FilestubeAPI = require('../index');
 var assert = require('assert');
 var nock = require('nock');
 
-var list;
-var final;
-
-var initMock = function(list, final){
+var initMock = function(list, final) {
   nock('http://www.filestube.to')
   .get('/query.html?q=stawka+wieksza+niz+zycie')
-  .reply(200, list)
-  .get('/details.html')
-  .reply(200, final)
-  .persist();
-};
+  .reply(200, list);
 
-var teardown = function(){
-  nock.restore();
+  nock('http://www.filestube.to')
+  .get('/details.html')
+  .reply(200, final);
 };
 
 //var elementsOnPage = 5;
 
 // getOne result with only one file
-list = require('./mocks/list-one-page.js');
-final = require('./mocks/final-single-link');
-initMock(list, final);
+initMock(
+  require('./mocks/list-one-page'),
+  require('./mocks/final-single-link')
+);
 FilestubeAPI.getOne('stawka wieksza niz zycie', {}, function(data){
   assert.equal(data.length, 1);
   assert.equal(
     data[0],
     'http://blablablabla.jczc/files/w2q9vfcl/Zbych.Andrzej-Stawka.wieksza.niz'
   );
-
-  teardown();
 });
+
+// getOne result with multiple files
+// initMock(
+//   require('./mocks/list-one-page'),
+//   require('./mocks/final-multiple-links')
+// );
+// FilestubeAPI.getOne('stawka wieksza niz zycie', {}, function(data){
+//   assert.equal(data.length, 5);
+// });
---
+++ tests/mocks/final-multiple-links.js
@@ -0,0 +1,11 @@
+'use script';
+
+// Final file with one single link
+module.exports =
+  '<span id="copy_paste_links">' +
+    'http://blablabla.jczc/files/w2q9vfcl/ZbychAndrzej-Stawka.wieksza.niz\r\n' +
+    'http://blablabla.jczc/files/w2q9vfcl/ZbychAndrzej-Stawka.wieksza.niz\r\n' +
+    'http://blablabla.jczc/files/w2q9vfcl/ZbychAndrzej-Stawka.wieksza.niz\r\n' +
+    'http://blablabla.jczc/files/w2q9vfcl/ZbychAndrzej-Stawka.wieksza.niz\r\n' +
+    'http://blablabla.jczc/files/w2q9vfcl/ZbychAndrzej-Stawka.wieksza.niz\r\n' +
+  '</span>';
