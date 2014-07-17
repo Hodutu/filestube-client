2ba4d8f3a9e3fd0230e4ff0efbba1e63197707cf
Michal Budzynski <michal@virtualdesign.pl>
2014-07-17 17:27:03 +0200

    [FILESTUBE-CLIENT] - :dragon: first tests finished

--- tests/filestube-client-test.js
+++ tests/filestube-client-test.js
@@ -1,7 +1,46 @@
 'use strict';
 
 var FilestubeAPI = require('../index');
+var assert = require('assert');
+var nock = require('nock');
 
-FilestubeAPI.getOne('Warsaw Photos', {}, function(data){
-  console.log(data);
+var list;
+var final;
+
+var initMock = function(list, final){
+  nock('http://www.filestube.to')
+  .get('/query.html?q=stawka+wieksza+niz+zycie')
+  .reply(200, list)
+  .get('/details.html')
+  .reply(200, final)
+  .persist();
+};
+
+var teardown = function(){
+  nock.restore();
+};
+
+var elementsOnPage = 5;
+
+// getOne result with only one file
+list = require('./mocks/list-one-page.js');
+final = require('./mocks/final-single-link');
+initMock(list, final);
+FilestubeAPI.getOne('stawka wieksza niz zycie', {}, function(data){
+  assert.equal(data.length, 1);
+  assert.equal(
+    data[0],
+    'http://blablablabla.jczc/files/w2q9vfcl/Zbych.Andrzej-Stawka.wieksza.niz'
+  );
+
+  teardown();
 });
+
+// // getOne result with only one file
+// list = require('./mocks/list-one-page.js');
+// final = require('./mocks/final-multiple-links')
+// initMock(require(list, final);
+// FilestubeAPI.getOne('stawka wieksza niz zycie', {}, function(data){
+//   assert.equal(data[0], 'http://blablablabla.jczc/files/w2q9vfcl/Zbych.Andrzej-Stawka.wieksza.niz');
+// });
+// teardown();
---
+++ tests/mocks/list-one-page.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports =
+  '<div class="r">' +
+    '<a href="http://www.filestube.to/details.html" class="rL">' +
+      'Zbych Andrzej - Stawka wieksza niz zycie Cz 17 Akcja Lisc Debu' +
+    '</a>' +
+  '</div>' +
+
+  '<div class="r">' +
+    '<a href="http://www.filestube.to/details.html" class="rL">' +
+      'Zbych Andrzej - Stawka wieksza niz zycie Cz 17 Akcja Lisc Debu' +
+    '</a>' +
+  '</div>' +
+
+  '<div class="r">' +
+    '<a href="http://www.filestube.to/details.html" class="rL">' +
+      'Zbych Andrzej - Stawka wieksza niz zycie Cz 17 Akcja Lisc Debu' +
+    '</a>' +
+  '</div>' +
+
+  '<div class="r">' +
+    '<a href="http://www.filestube.to/details.html" class="rL">' +
+      'Zbych Andrzej - Stawka wieksza niz zycie Cz 17 Akcja Lisc Debu' +
+    '</a>' +
+  '</div>' +
+
+  '<div class="r">' +
+    '<a href="http://www.filestube.to/details.html" class="rL">' +
+      'Zbych Andrzej - Stawka wieksza niz zycie Cz 17 Akcja Lisc Debu' +
+    '</a>' +
+  '</div>';
