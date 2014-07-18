9b18bd9e6b211f5bebf79bcf129fab775a6a51f3
Michal Budzynski <michal@virtualdesign.pl>
2014-07-18 11:57:27 +0200

    [FILESTUBE-CLIENT] - :family: new mock for tests

--- tests/mocks/init-mock.js
+++ tests/mocks/init-mock.js
@@ -1,13 +1,16 @@
 'use strict';
 
-var nock = require('nock');
+var Nock = require('nock');
+var nock = Nock('http://www.filestube.to');
 
-module.exports = function(list, final) {
-  nock('http://www.filestube.to')
-  .get('/query.html?q=stawka+wieksza+niz+zycie')
+
+
+var initMock = function(list, final) {
+  nock.get('/query.html?q=stawka+wieksza+niz+zycie')
   .reply(200, list);
 
-  nock('http://www.filestube.to')
-  .get('/details.html')
+  nock.get('/details.html').once()
   .reply(200, final);
 };
+
+module.exports = initMock;
