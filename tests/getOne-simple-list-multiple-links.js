381b2430189fa2cd3bc6a4825d71fc42dcea89ef
Michal Budzynski <michal@virtualdesign.pl>
2014-07-18 11:58:08 +0200

    [FILESTUBE-CLIENT] - :angel: tests environment setup

---
+++ tests/getOne-simple-list-multiple-links.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var TESTHELPER = require('./helper');
+var assert = require('assert');
+
+// getOne result with multiple files
+module.exports = {
+  run: function(){
+    TESTHELPER({
+      list: require('./mocks/list-one-page'),
+      final: require('./mocks/final-multiple-links'),
+      testedMethod: 'getOne',
+      options: {},
+      assert: function(data){
+        assert.equal(data.length, 5);
+      }
+    });
+  }
+};
