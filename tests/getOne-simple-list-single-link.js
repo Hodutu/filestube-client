9834ea5a8e6f0102497e8a07d0fa1ae6a7aee6b4
Michal Budzynski <michal@virtualdesign.pl>
2014-07-18 11:26:31 +0200

    [FILESTUBE-CLIENT] - :aerial_tramway: separate tests into own files

---
+++ tests/getOne-simple-list-single-link.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var TESTHELPER = require('./helper');
+var assert = require('assert');
+
+// getOne result with only one file
+module.exports = {
+  run: function(){
+    TESTHELPER({
+      list: require('./mocks/list-one-page'),
+      final: require('./mocks/final-single-link'),
+      testedMethod: 'getOne',
+      options: {},
+      assert: function(data){
+        assert.equal(data.length, 1);
+        assert.equal(
+          data[0],
+          'http://blablabla.jczc/file/w2q9vfcl/Zbych.Andrzej-Stawka.wieksza.niz'
+        );
+      }
+    });
+  }
+};
